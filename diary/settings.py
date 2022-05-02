1b951549c3e1ed422518db8dfe400294bde36a46
adhi3110 <33823041+adhi3110@users.noreply.github.com>
2022-05-02 23:17:19 +0530

    added rest api and auth

--- diary/settings.py
+++ diary/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
+    'registerUser',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +132,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # <-- And here
+    ],
+}
