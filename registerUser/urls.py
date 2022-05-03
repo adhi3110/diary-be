7f872e82aaed03759efa38819f449383ded59dc0
adhi3110 <33823041+adhi3110@users.noreply.github.com>
2022-05-03 13:02:24 +0530

    testing apis with angular

--- registerUser/urls.py
+++ registerUser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.register),
     path('hello/', views.HelloView.as_view(), name='hello'),
 ]
--- registerUser/views.py
+++ registerUser/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated  # <-- Here
 
 
@@ -9,3 +10,13 @@ class HelloView(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        name = request.data['name']
+        if name:
+            return Response({'name': name})
+        else:
+            return Response({'hell': name})
